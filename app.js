9644ed9c71d34cca40c8082abd398cf72873fdf0
sjkeskar <sjkeskar@gmail.com>
2020-08-16 20:46:53 +0530

    button event listener added, keypress not done

---
+++ app.js
@@ -0,0 +1,45 @@
+const btnum = document.querySelectorAll(".drum").length;
+for(var i=0;i<btnum;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",justtried);
+}
+
+function justtried() {
+    const clicked = this.innerText;
+    switch(clicked){
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            console.log("tom-1");
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            console.log("tom-2");
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            console.log("tom-3");
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            console.log("tom-4");
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            console.log("snare");
+            break;
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            console.log("crash");
+            break;
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            console.log("bass");
+            break;
+    }
+}
--- index.js
+++
@@ -1,6 +0,0 @@
-var btn1 = document.querySelector("button");
-btn1.addEventListener("click",alertme);
-
-function alertme(){
-    alert("Click registered");
-}
